d3a6d2ffe09056f99c57e3c051d3d4d8038af1fe
sakute <andriy.erdek1012@gmail.com>
2026-03-31 22:31:49 +0300

    ready for deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,15 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })
-
     ->withMiddleware(function (Middleware $middleware) {
+        // 1. Довіряємо проксі Render (це вирішує проблему з сесіями)
         $middleware->trustProxies(at: '*');
-    })
 
+        // 2. Дозволяємо передачу сесій через HTTPS
+        $middleware->statefulApi();
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
     ->create();
